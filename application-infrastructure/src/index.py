b1566874d5e7b30ec206051c06d38026b7b78e19
Chad Kluck <clkluck@stthomas.edu>
2025-11-07 20:50:50 -0600

    updating to include file name in directory structure

--- application-infrastructure/src/index.py
+++ application-infrastructure/src/index.py
@@ -4,6 +4,28 @@ from urllib.parse import urlparse
 import uuid
 import boto3
 
+VIDEO_OUTPUT_BUCKET = os.environ["VIDEO_OUTPUT_BUCKET"]
+VIDEO_OUTPUT_PREFIX = os.environ["VIDEO_OUTPUT_PREFIX"]
+AWS_LAMBDA_FUNCTION_NAME = os.environ["AWS_LAMBDA_FUNCTION_NAME"]
+AWS_REGION = os.environ["AWS_DEFAULT_REGION"]
+MEDIA_CONVERT_ROLE = os.environ['MEDIA_CONVERT_ROLE']
+
+def normalize_output_prefix(object_key, etag=None):
+    """Normalize the output prefix by replacing invalid characters."""
+
+    output_prefix = os.path.splitext(os.path.basename(object_key))[0]
+    output_prefix = ''.join(c if c.isalnum() or c in '-_' else '_' for c in output_prefix)
+    output_prefix = '_'.join(filter(None, output_prefix.split('_')))
+    output_prefix = output_prefix.strip('_')
+    output_prefix = output_prefix.strip('-')
+    if etag is None:
+        etag = str(uuid.uuid4())
+        etag = etag.replace('-', '')
+
+    output_prefix = output_prefix + '-' + etag[:8]
+
+    return output_prefix
+
 """
 When you run an S3 Batch Operations job, your job  
 invokes this Lambda function. Specifically, the Lambda function is 
@@ -25,12 +47,19 @@ def handler(event, context):
     source_s3_key = event['Records'][0]['s3']['object']['key']
     source_s3_bucket = event['Records'][0]['s3']['bucket']['name']
     source_s3 = 's3://' + source_s3_bucket + '/' + source_s3_key
+    source_etag = event['Records'][0]['s3']['object']['eTag']
     
     request_id = event['Records'][0]['responseElements']['x-amz-request-id']
     user_id = event['Records'][0]['userIdentity']['principalId']
     user_ip = event['Records'][0]['requestParameters']['sourceIPAddress']
     
     print('Processing New Object: ' + source_s3)
+
+    output_prefix = normalize_output_prefix(source_s3_key, source_etag)
+
+    destination_s3 = f's3://{VIDEO_OUTPUT_BUCKET}{VIDEO_OUTPUT_PREFIX}{output_prefix}'
+
+    print('Destination: ' + destination_s3)
 
     result_list = []
     result_code = 'Succeeded'
@@ -54,14 +83,6 @@ def handler(event, context):
 
         job_settings['Inputs'][0]['FileInput'] = source_s3
 
-        # Use the name of the source_s3 file as the name of the output prefix
-        # For example if we receive s3://mybucket/uploads/my-video.mp4 then create the prefix my-video
-        output_prefix = os.path.splitext(os.path.basename(source_s3))[0]
-
-        destination_s3 = f's3://{os.environ["VIDEO_OUTPUT_BUCKET"]}{os.environ["VIDEO_OUTPUT_PREFIX"]}/{output_prefix}'
-
-        print('Destination: ' + destination_s3)
-
         # The path of each output video is constructed based on the values of 
         # the attributes in each object of OutputGroups in the job.json file. 
 
@@ -78,20 +99,19 @@ def handler(event, context):
 
         job_metadata_dict = {
             'assetID': str(uuid.uuid4()),
-            'application': os.environ['AWS_LAMBDA_FUNCTION_NAME'],
+            'application': AWS_LAMBDA_FUNCTION_NAME,
             'input': source_s3,
             'settings': job_name
         }
         
-        region = os.environ['AWS_DEFAULT_REGION']
-        endpoints = boto3.client('mediaconvert', region_name=region) \
+        endpoints = boto3.client('mediaconvert', region_name=AWS_REGION) \
             .describe_endpoints()
-        client = boto3.client('mediaconvert', region_name=region, 
+        client = boto3.client('mediaconvert', region_name=AWS_REGION, 
                                endpoint_url=endpoints['Endpoints'][0]['Url'], 
                                verify=True)
                                
         try:
-            client.create_job(Role=os.environ['MEDIA_CONVERT_ROLE'], 
+            client.create_job(Role=MEDIA_CONVERT_ROLE, 
                               UserMetadata=job_metadata_dict, 
                               Settings=job_settings)
         # You can customize error handling based on different error codes that 
